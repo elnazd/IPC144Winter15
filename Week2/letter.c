60f0ea302fc893b42ae2b3c2167daf3b28ac7856
elnazd <elnaz.delpisheh@gmail.com>
2015-01-23 12:15:51 -0500

    Enter a letter

---
+++ Week2/letter.c
@@ -0,0 +1,11 @@
+ //Write a program to accept a letter from a user.
+ #include<stdio.h>
+ int main()
+ {
+	char letter;
+	printf("Please input a letter:");
+	scanf("%c",&letter);
+
+	printf("The letter you input is:%c\n",letter); 
+ 
+ }
