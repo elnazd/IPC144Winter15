104f9c033f1ec2d0969160e8976218037b7cf41a
elnazd <elnaz.delpisheh@gmail.com>
2015-01-26 21:42:49 -0500

    Relation expressions

---
+++ week3/walkthrough1.c
@@ -0,0 +1,13 @@
+//What is the output of the following cod:
+
+#include <stdio.h>
+
+int main()
+{
+  int a = 7;
+  int b = 7;
+
+  printf("%d\n%d\n%d\n%d\n%d\n%d\n", a==b, a!=b, a<b, a<=b, a>b, a>=b);
+
+  return 0;
+}
